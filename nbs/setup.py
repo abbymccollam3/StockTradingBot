03146f6aa8864b69b4424c3604c29476d6cdfbb1
abbymccollam3 <abby.mccollam@gmail.com>
2024-11-19 20:35:42 -0800

    setup.py

---
+++ nbs/setup.py
@@ -0,0 +1,20 @@
+
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+import pathlib
+
+THIS_FILE_PATH = pathlib.Path(__file__).resolve() # file path
+NBS_DIR = THIS_FILE_PATH.parent
+REPO_DIR = THIS_FILE_PATH.parent
+DJANGO_BASE_DIR = REPO_DIR / "src"
+DJANGO_PROJECT_SETTINGS_NAME = 'cfehome'
+
+def init_django():
+    """Run administrative tasks."""
+    os.chdir(DJANGO_BASE_DIR) # root directory of project we will be working on
+    sys.path.insert(0, str(DJANGO_BASE_DIR))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfehome.settings')
+    
+    import django
+    django.setup()
