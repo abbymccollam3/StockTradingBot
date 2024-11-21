182cb1e2662706bb785a997a1b8ea32e50baa2d4
“abbymccollam3” <amccollam3@gatech.edu>
2024-11-20 23:25:12 -0800

    models.py

--- src/market/models.py
+++ src/market/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=120)
+    ticker = models.CharField(max_length=20, unique=True, db_index=True)
+    description = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updates = models.DateTimeField(auto_now=True)
+
+# defining database 
+
+# stock market tickers can change
+
+class StockQuote(models.Model):
+
+    """
+    'open_price': result['o'],
+    'close_price': result['c'],
+    'high_price': result['h'],
+    'low_price': result['l'],
+    'number_of_trades': result['n'],
+    'volume': result['v'],
+    'volume_weighted_average': result['vw'],
+    'time': utc_timestamp,
+    """
+
+    company = models.ForeignKey(
+        Company,
+        on_delete = models.CASCADE, # what do we do when a company is deleted
+        related_name="stock_quotes"
+    )
+    open_price = models.DecimalField(max_digits=10, decimal_places=4)
+    close_price = models.DecimalField(max_digits=10, decimal_places=4)
+    high_price = models.DecimalField(max_digits=10, decimal_places=4)
+    low_price = models.DecimalField(max_digits=10, decimal_places=4)
+    number_of_trades = models.BigIntegerField(blank=True, null=True)
+    volume = models.BigIntegerField()
+    volume_weighted_average = models.DecimalField(max_digits=10, decimal_places=4)
