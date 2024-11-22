c5213deca4b4609acc59d79d5a6a680dc4bf7784
“abbymccollam3” <amccollam3@gatech.edu>
2024-11-21 20:42:02 -0800

    test

--- src/market/models.py
+++ src/market/models.py
@@ -5,7 +5,7 @@ class Company(models.Model):
     name = models.CharField(max_length=120)
     ticker = models.CharField(max_length=20, unique=True, db_index=True)
     description = models.TextField(blank=True, null=True)
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default=True) 
     timestamp = models.DateTimeField(auto_now_add=True)
     updates = models.DateTimeField(auto_now=True)
 
