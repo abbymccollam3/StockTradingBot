0a829e55974294beda84d6b47b9fa0292a8a790f
“abbymccollam3” <amccollam3@gatech.edu>
2024-11-25 09:26:11 -0800

    utils and tasks

--- src/market/utils.py
+++ src/market/utils.py
@@ -1,5 +1,6 @@
 from django.apps import apps
 
+
 def batch_insert_stock_data(dataset, company_obj=None, batch_size=1000):
     StockQuote = apps.get_model('market', 'StockQuote')
     batch_size = 1000
@@ -14,18 +15,4 @@ def batch_insert_stock_data(dataset, company_obj=None, batch_size=1000):
             )
         
         StockQuote.objects.bulk_create(chunked_quotes, ignore_conflicts=True)
-
-def sync_company_stock_quotes(company_id):
-    Company = apps.get_model("market", "Company")
-    try:
-        company_obj = Company.objects.get(id=company_id)
-    except:
-        company_obj = None
-    if company_obj is None:
-        return
-
-def sync_stock_data():
-    Company = apps.get_model("market", "Company")
-    companies = Company.objects.filter(active=True).values_list('id')
-    for company_id in companies:
-        sync_company_stock_quotes(company_id)
+    return len(dataset)
