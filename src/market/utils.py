6b4f2aef9430e2a3f99e98f815df24d0c1853dc8
“abbymccollam3” <amccollam3@gatech.edu>
2024-11-25 10:00:09 -0800

    tasks and utils

--- src/market/utils.py
+++ src/market/utils.py
@@ -1,12 +1,14 @@
 from django.apps import apps
 
 
-def batch_insert_stock_data(dataset, company_obj=None, batch_size=1000):
+def batch_insert_stock_data(dataset, company_obj=None, batch_size=1000, verbose=False):
     StockQuote = apps.get_model('market', 'StockQuote')
     batch_size = 1000
     if company_obj is None:
         return
     for i in range(0, len(dataset), batch_size):
+        if verbose:
+            print("Doing chunk", i)
         batch_chunk = dataset [i:i+batch_size]
         chunked_quotes = []
         for data in batch_chunk:
@@ -15,4 +17,6 @@ def batch_insert_stock_data(dataset, company_obj=None, batch_size=1000):
             )
         
         StockQuote.objects.bulk_create(chunked_quotes, ignore_conflicts=True)
+        if verbose:
+            print("Finished chunk", i)
     return len(dataset)
