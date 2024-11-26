63832ba33cbcdc9ef43683972589d9929b0f3e18
abbymccollam3 <abby.mccollam@gmail.com>
2024-11-25 16:16:23 -0800

    tasks and utils

--- src/market/tasks.py
+++ src/market/tasks.py
@@ -20,9 +20,6 @@ def sync_company_stock_quotes(company_id, days_ago = 32, date_format = "%Y-%m-%d
         raise Exception(f"Company ticker: {company_ticker} is invalid")
     end_date = timezone.now()
     start_date = end_date - timedelta(days=days_ago)
-
-
-
     to_date = end_date.strftime(date_format)
     from_date = start_date.strftime(date_format)
     client = helper_clients.PolygonAPIClient(
