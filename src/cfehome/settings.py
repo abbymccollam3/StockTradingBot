9abe6611a8a77ba6684862d9c8b1fa410d2fb53f
abbymccollam3 <abby.mccollam@gmail.com>
2024-11-25 22:33:12 -0800

    settings

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -136,3 +136,9 @@ REDIS_URL = config("REDIS_URL", default = 'redis://localhost:6379')
 CELERY_BROKER_URL = REDIS_URL
 CELERY_RESULT_BACKEND = "django-db"
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers.DatabaseScheduler"
+
+# more celery configs
+CELERY_BROKER_CONNECTION_RETRY = True
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+CELERY_REDIS_BACKEND_USE_SSL = False
+CELERY_BROKER_USE_SSL = False
