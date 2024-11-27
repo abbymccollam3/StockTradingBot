17b5cf0a0cb39212db6218c586dad42466070c6c
“abbymccollam3” <amccollam3@gatech.edu>
2024-11-26 21:09:22 -0800

    admin.py

--- src/market/admin.py
+++ src/market/admin.py
@@ -7,10 +7,13 @@ admin.site.register(Company)
 
 # this adds data to Django and allows you to see the stock quotes that are currently available
 class StockQuoteAdmin(admin.ModelAdmin):
-    list_display=['company__ticker', 'close_price', 'time']
+    list_display=['company__ticker', 'close_price', 'localized_time']
     list_filter = ['company__ticker', 'time']
 
-    class Meta:
-        model = StockQuote
+    def localized_time(self, obj):
+        return obj.time.strftime("%b %d, %Y, %I:%M %p (%Z)")
+
+    # class Meta:
+    #     model = StockQuote
 
 admin.site.register(StockQuote, StockQuoteAdmin)
