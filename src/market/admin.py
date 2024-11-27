53cd6235ec5523796371743b92f05f28c0097580
abbymccollam3 <abby.mccollam@gmail.com>
2024-11-26 16:44:39 -0800

    admin

--- src/market/admin.py
+++ src/market/admin.py
@@ -5,6 +5,7 @@ from .models import StockQuote, Company
 
 admin.site.register(Company)
 
+# this adds data to Django and allows you to see the stock quotes that are currently available
 class StockQuoteAdmin(admin.ModelAdmin):
     list_display=['company__ticker', 'close_price', 'time']
     list_filter = ['company__ticker', 'time']
